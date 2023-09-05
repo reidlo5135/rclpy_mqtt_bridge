b01876c48c43cf1acb6c89c85caaafb08dcc9b91
reidlo5135 <201840103@daelim.ac.kr>
2023-09-05 11:52:09 +0900

    chore : apply pydoc comments

--- src/rclpy_mqtt_bridge/main.py
+++ src/rclpy_mqtt_bridge/main.py
@@ -5,6 +5,24 @@ from .bridge.dynamic_bridge import dynamic_bridge
 
 
 def main(args=None) -> None:
+    
+    """ Description
+        
+    Main methods for this program.
+    
+    Initialize rclpy with default args and spin dynamic_bridge that is rclpy node.
+        
+    Args:
+        - args: Main arguments (None)
+
+    Returns:
+        None
+            
+    Usage:
+        if __name__ == "__main__":
+            main()
+    """
+    
     rclpy.init(args=args)
 
     try:
